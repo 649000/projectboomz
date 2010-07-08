a3a97ad4e117138a3fe4ef35984749fabd4a054f
nazri610 <nazri610@3847c4ab-4fc7-fcde-3ce7-b0303817ffbf>
2010-07-08 13:53:44 +0000

    LevelController

--- trunk/ProjectBoomz/grails-app/controllers/app/LevelController.groovy
+++ trunk/ProjectBoomz/grails-app/controllers/app/LevelController.groovy
@@ -1,15 +1,19 @@
 package app
 
+import grails.converters.JSON
+
 class LevelController {
 
     def index = { 
-        println(params.name)
-        render params.name
-
         //Retrieve building info based on name
-        //Display building info and germanium 3D model of the building
+        //def _building = Building.findByBuildingName(params.name);
 
+       // println(_building.type)
+
+        //Display building info and germanium 3D model of the building
+        //render as JSON, the appropriate model based on the model
         
+        //render _building as JSON
 
     }
 }
