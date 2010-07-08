a249dbf24a7772195112077b30ae72668d6386f4
yuanjie91 <yuanjie91@3847c4ab-4fc7-fcde-3ce7-b0303817ffbf>
2010-07-08 05:22:39 +0000

    JavaScript calls to Google Map's JavaScript API v2 modified for search function.

--- ProjectBoomz/web-app/js/scripts.js
+++ ProjectBoomz/web-app/js/scripts.js
@@ -4,7 +4,7 @@ function load(response)
   if (GBrowserIsCompatible())
   {
     var map = new GMap2(document.getElementById("map"));
-    map.setCenter(new GLatLng(1.300931,103.838503), 16);
+    map.setCenter(new GLatLng(1.358058,103.879166), 11);
     map.addControl(new GLargeMapControl3D());
     map.addControl(new GMapTypeControl());
 
@@ -19,9 +19,10 @@ function addPins(map, response)
     for (var i = 0; i < results.length; i++)
     {
         var marker = new GMarker(   new GLatLng( results[i].latitude, results[i].longitude )   )
-        marker.bindInfoWindowHtml( "<font face=\"Arial\" size=\"2em\"><b>" + results[i].buildingName + "</b><br/>" +  +
+        marker.bindInfoWindowHtml( "<font face=\"Arial\" size=\"2em\"><b>" + results[i].buildingName + "</b>" +
                                     "<br/><br/><b>Type:</b><br/>" + results[i].type +
-                                    "<br/><br/><b>Noise Level:</b><br/>" + results[i].noiseLevel + "</font>")
+                                    "<br/><br/><b>Noise Level:</b><br/>" + results[i].noiseLevel +
+                                    "<br/><br/><table width=\"100%\"><tr><td><b><a href=\"#\">Share</a></b></td><td><b><a href=\"#\">View</a></b></td></tr></table></font>")
         map.addOverlay(marker)
     }
     
