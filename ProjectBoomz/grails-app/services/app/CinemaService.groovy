2015f893870c25ae46d8f8fdb5f2e4f493cb3fc9
nazri610 <nazri610@3847c4ab-4fc7-fcde-3ce7-b0303817ffbf>
2010-07-08 03:37:10 +0000

    Services

--- ProjectBoomz/grails-app/services/app/CinemaService.groovy
+++ ProjectBoomz/grails-app/services/app/CinemaService.groovy
@@ -7,4 +7,42 @@ class CinemaService {
     def serviceMethod() {
 
     }
+    
+    def ArrayList getCinemas()
+    {
+        def retrievedContent;
+
+        def buildingList = new ArrayList<Building>()
+
+        try {
+            URL _url = new URL("http://api.projectnimbus.org/coodataservice.svc/CinemaSet");
+            URLConnection _urlConn = _url.openConnection();
+
+            _urlConn.setRequestProperty("accept", "*/*");
+
+            _urlConn.addRequestProperty("AccountKey", "Xu584Q7QbYo+/bBO43SWmvhqdf0=");
+            _urlConn.addRequestProperty("UniqueUserID", "98765432145269875154125015201452");
+
+            retrievedContent = _urlConn.content.text
+
+            def xml = new XmlSlurper().parseText(retrievedContent);
+
+            xml.entry.each {
+                def buildingz = new Building()
+                buildingz.buildingName = it.content.properties.Name.text()
+                buildingz.longitude = it.content.properties.Longitude.text()
+                buildingz.latitude = it.content.properties.Latitude.text()
+                buildingz.type = "Cinema"
+                buildingList.add(buildingz)
+            }
+
+        } catch (Exception e) {
+            println(e);
+        }
+
+
+       // println(retrievedContent);
+        return buildingList
+
+    }
 }
--- ProjectBoomz/grails-app/services/app/PlacesOfInterestService.groovy
+++ ProjectBoomz/grails-app/services/app/PlacesOfInterestService.groovy
@@ -7,4 +7,41 @@ class PlacesOfInterestService {
     def serviceMethod() {
 
     }
+    def ArrayList getPlaces()
+    {
+        def retrievedContent;
+
+        def buildingList = new ArrayList<Building>()
+
+        try {
+            URL _url = new URL("http://api.projectnimbus.org/stbodataservice.svc/PlaceSet");
+            URLConnection _urlConn = _url.openConnection();
+
+            _urlConn.setRequestProperty("accept", "*/*");
+
+            _urlConn.addRequestProperty("AccountKey", "Xu584Q7QbYo+/bBO43SWmvhqdf0=");
+            _urlConn.addRequestProperty("UniqueUserID", "98765432145269875154125015201452");
+
+            retrievedContent = _urlConn.content.text
+
+            def xml = new XmlSlurper().parseText(retrievedContent);
+
+            xml.entry.each {
+                def buildingz = new Building()
+                buildingz.buildingName = it.content.properties.Name.text()
+                buildingz.longitude = it.content.properties.Longitude.text()
+                buildingz.latitude = it.content.properties.Latitude.text()
+                buildingz.type = "Places of Interest"
+                buildingList.add(buildingz)
+            }
+
+        } catch (Exception e) {
+            println(e);
+        }
+
+
+       // println(retrievedContent);
+        return buildingList
+
+    }
 }
