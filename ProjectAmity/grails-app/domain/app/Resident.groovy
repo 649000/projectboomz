7abbd1bec61d2d749ee3312be2db9b0590213b1b
yuanjie91 <yuanjie91@3847c4ab-4fc7-fcde-3ce7-b0303817ffbf>
2010-07-13 05:52:02 +0000

    Changed attributes of this class to model the Driver relationship more closely.

--- ProjectAmity/grails-app/domain/app/Resident.groovy
+++ ProjectAmity/grails-app/domain/app/Resident.groovy
@@ -2,14 +2,16 @@ package app
 
 class Resident {
 
-    static constraints = {
-    }
+
     String nric
     String name
     String address
     String userid
     String password
     static hasMany = [ passengers : Resident, cabSharers : Resident, barterAds : BarterAd  ]
-    static belongsTo = [ driver : Resident ]
+    static hasOne = [ driver : Resident ]
+
+    static constraints = {
+    }
 
 }
