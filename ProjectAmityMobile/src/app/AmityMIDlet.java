3873e066195fc520a4ef26a1d78eb2eda523c36d
nazri610 <nazri610@3847c4ab-4fc7-fcde-3ce7-b0303817ffbf>
2010-07-20 15:04:47 +0000

    God Bless Us.

--- ProjectAmityMobile/src/app/AmityMIDlet.java
+++ ProjectAmityMobile/src/app/AmityMIDlet.java
@@ -15,9 +15,8 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Date;
 import java.util.Vector;
-import org.apache.commons.codec.binary.Base64;
 import javax.microedition.io.file.FileConnection;
-
+//import org.apache.commons.codec.*;
 
 /**
  * @author student
@@ -26,23 +25,26 @@ public class AmityMIDlet extends MIDlet implements CommandListener, ItemCommandL
 
     private boolean midletPaused = false;
     private LocationProvider provider = null;
-    private String userIDLoggedIn = "";
+    private String userIDLoggedIn = "a";
     private String userPostalCode = "";
     //URLs
     private String loginUserURL = "http://localhost:8080/ProjectAmity/resident/mLogin";
     private String reportOutdoorURL = "http://localhost:8080/ProjectAmity/report/saveOutdoor";
     private String reportIndoorURL = "http://localhost:8080/ProjectAmity/report/saveIndoor";
     private String getPostalcode = "http://localhost:8080/ProjectAmity/resident/mPostalCode";
+    //Need to get building info, level and stairwell
+    private String buildingInfoURL = "";
     //Server return messages
     private String loginServerMsg = "";
     private String reportOutdoorServerMsg = "";
     private String reportIndoorServerMsg = "";
     //temp variables to store the GPS coordinates
-    private double latitude = 0;
-    private double longitude = 0;
+    private double latitude = 0.0;
+    private double longitude = 0.0;
     private float altitude = 0;
     private byte[] imageByteOutdoor;
     private byte[] imageByteIndoor;
+    private String imageName = "";
     private Player mPlayer;
     private VideoControl mVideoControl;
     private Display mDisplay;
@@ -176,6 +178,16 @@ public class AmityMIDlet extends MIDlet implements CommandListener, ItemCommandL
                 // write post-action user code here
             } else if (command == indoorReportSubmitCommand) {//GEN-LINE:|7-commandAction|5|58-preAction
                 // write pre-action user code here
+                if (!indoorTitleTextField.getString().equals("") && !indoorDescriptiontextField.getString().equals("")) { //&& indoorImageItem.getImage() != null) {
+                    reportSubmitIndoor();
+                } else {
+
+                    System.out.println("Empty Fields Detected");
+                    alert = new Alert("Error", "Invalid Entry. \nEmpty Fields detected", null, AlertType.ERROR);
+                    alert.setTimeout(2000); //Timeout in 2 seconds
+                    switchDisplayable(alert, getIndoorReportForm());
+                }
+
 //GEN-LINE:|7-commandAction|6|58-postAction
                 // write post-action user code here
             }//GEN-BEGIN:|7-commandAction|7|21-preAction
@@ -229,10 +241,17 @@ public class AmityMIDlet extends MIDlet implements CommandListener, ItemCommandL
                 switchDisplayable(null, getReportMainForm());//GEN-LINE:|7-commandAction|14|50-postAction
                 // write post-action user code here
             } else if (command == outdoorReportSubmitCommand) {//GEN-LINE:|7-commandAction|15|56-preAction
-                // write pre-action user code here
+                if (!outdoorTitletextField.getString().equals("") && !outdoorDescriptiontextField.getString().equals("")) { //&& latitude != 0.0 && longitude != 0.0&& outdoorImageItem.getImage() != null) {
+                    reportSubmitOutdoor();
+                } else {
 
-                //Validation to make sure that the fields are not empty
-                reportSubmitOutdoor();
+                    System.out.println("Empty Fields Detected");
+                    alert = new Alert("Error", "Invalid Entry. \nEmpty Fields detected and/or Invalid GPS Coordinates.", null, AlertType.ERROR);
+                    alert.setTimeout(2000); //Timeout in 2 seconds
+                    switchDisplayable(alert, getOutdoorReportForm());
+                }
+
+
 //GEN-LINE:|7-commandAction|16|56-postAction
                 // write post-action user code here
             }//GEN-BEGIN:|7-commandAction|17|77-preAction
@@ -640,7 +659,7 @@ public class AmityMIDlet extends MIDlet implements CommandListener, ItemCommandL
     public Command getIndoorReportSubmitCommand() {
         if (indoorReportSubmitCommand == null) {//GEN-END:|57-getter|0|57-preInit
             // write pre-init user code here
-            indoorReportSubmitCommand = new Command("Submit", "<null>", Command.OK, 0);//GEN-LINE:|57-getter|1|57-postInit
+            indoorReportSubmitCommand = new Command("Submit", "Submit", Command.OK, 0);//GEN-LINE:|57-getter|1|57-postInit
             // write post-init user code here
         }//GEN-BEGIN:|57-getter|2|
         return indoorReportSubmitCommand;
@@ -949,6 +968,7 @@ public class AmityMIDlet extends MIDlet implements CommandListener, ItemCommandL
         }
         return result;
     }
+
     static public String urlEncode(String sUrl) {
 
         //Url Encoding for period has been omitted
@@ -984,6 +1004,54 @@ public class AmityMIDlet extends MIDlet implements CommandListener, ItemCommandL
         }
         return urlOK.toString();
     }
+    private static char[] map1 = new char[64];
+
+    static {
+        int i = 0;
+        for (char c = 'A'; c <= 'Z'; c++) {
+            map1[i++] = c;
+        }
+        for (char c = 'a'; c <= 'z'; c++) {
+            map1[i++] = c;
+        }
+        for (char c = '0'; c <= '9'; c++) {
+            map1[i++] = c;
+        }
+        map1[i++] = '+';
+        map1[i++] = '/';
+    }
+
+    public static String base64Encode(byte[] in) {
+        int iLen = in.length;
+        int oDataLen = (iLen * 4 + 2) / 3;// output length without padding
+        int oLen = ((iLen + 2) / 3) * 4;// output length including padding
+        char[] out = new char[oLen];
+        int ip = 0;
+        int op = 0;
+        int i0;
+        int i1;
+        int i2;
+        int o0;
+        int o1;
+        int o2;
+        int o3;
+        while (ip < iLen) {
+            i0 = in[ip++] & 0xff;
+            i1 = ip < iLen ? in[ip++] & 0xff : 0;
+            i2 = ip < iLen ? in[ip++] & 0xff : 0;
+            o0 = i0 >>> 2;
+            o1 = ((i0 & 3) << 4) | (i1 >>> 4);
+            o2 = ((i1 & 0xf) << 2) | (i2 >>> 6);
+            o3 = i2 & 0x3F;
+            out[op++] = map1[o0];
+            out[op++] = map1[o1];
+            out[op] = op < oDataLen ? map1[o2] : '=';
+            op++;
+            out[op] = op < oDataLen ? map1[o3] : '=';
+            op++;
+        }
+        return new String(out);
+    }
 
     private void getLocation() {
         new Thread() {
@@ -1011,6 +1079,7 @@ public class AmityMIDlet extends MIDlet implements CommandListener, ItemCommandL
 
 
                     } else if (provider.getState() == LocationProvider.TEMPORARILY_UNAVAILABLE) {
+                        switchDisplayable(null, getOutdoorReportForm());
                         outdoorCoordinarteStringItem.setText("Unable to get coordinates");
                         System.out.println("Provider is unavailable");
                     }
@@ -1047,7 +1116,7 @@ public class AmityMIDlet extends MIDlet implements CommandListener, ItemCommandL
                     loginServerMsg = serverMsg.toString().trim();
                     is.close();
                     hc.close();
-                    String temp[] = split(loginServerMsg,"|");
+                    String temp[] = split(loginServerMsg, "|");
                     if (temp[0].equals("T")) {
 
                         loginServerMsg = "";
@@ -1082,15 +1151,16 @@ public class AmityMIDlet extends MIDlet implements CommandListener, ItemCommandL
                 InputStream is = null;
 
                 try {
-                    outdoorCoordinarteStringItem.setText("");
-                    hc = (HttpConnection) Connector.open(reportOutdoorURL + urlEncode("?userid=" + userIDLoggedIn + "&title=" + outdoorTitletextField.getString() + "&description=" + outdoorDescriptiontextField.getString() + "&latitude=" + latitude + "&longitude=" + longitude + "&altitude=" + altitude), Connector.READ_WRITE);
+                    
+                    hc = (HttpConnection) Connector.open(reportOutdoorURL + urlEncode("?userid=" + userIDLoggedIn + "&title=" + outdoorTitletextField.getString() + "&description=" + outdoorDescriptiontextField.getString() + "&latitude=" + latitude + "&longitude=" + longitude + "&altitude=" + altitude + "&imagename=" + imageName), Connector.READ_WRITE);
+
                     hc.setRequestMethod(HttpConnection.POST);
                     OutputStream out = hc.openOutputStream();
 
                     //Base64 b = new Base64();
-               //     String imageString = b.encodeToString(imageByteOutdoor);
-                  
-                  //  out.write(imageString.getBytes());
+                    //     String imageString = b.encodeToString(imageByteOutdoor);
+                    //  String imageString = base64Encode(imageByteOutdoor);
+                    //  out.write(imageString.getBytes());
 
                     is = hc.openInputStream();
                     int ch = is.read();
@@ -1101,16 +1171,17 @@ public class AmityMIDlet extends MIDlet implements CommandListener, ItemCommandL
                     }
                     System.out.println("Submit Report Outdoors THREAD: " + serverMsg.toString().trim());
                     reportOutdoorServerMsg = serverMsg.toString().trim();
-                    out.flush();
+                    // out.flush();
                     out.close();
                     is.close();
                     hc.close();
 
                     if (reportOutdoorServerMsg.equals("T")) {
-
+                    outdoorCoordinarteStringItem.setText("");
                         reportOutdoorServerMsg = "";
-                        //Show alert?
-                        switchDisplayable(null, getMainMenuForm());
+                        alert = new Alert("Success", "Report has been successfully submitted.", null, AlertType.CONFIRMATION);
+                        alert.setTimeout(2000); //Timeout in 2 seconds
+                        switchDisplayable(alert, getMainMenuForm());
                     }
 
                     if (reportOutdoorServerMsg.equals("F")) {
@@ -1134,9 +1205,57 @@ public class AmityMIDlet extends MIDlet implements CommandListener, ItemCommandL
         new Thread() {
 
             public void run() {
+
                 StringBuffer serverMsg = new StringBuffer("");
                 HttpConnection hc = null;
                 InputStream is = null;
+
+                try {
+                    
+                    hc = (HttpConnection) Connector.open(reportIndoorURL + urlEncode("?userid=" + userIDLoggedIn + "&title=" + indoorTitleTextField.getString() + "&description=" + indoorDescriptiontextField.getString() + "&postalCode=" + userPostalCode+ "&imagename=" + imageName), Connector.READ_WRITE);
+
+                    hc.setRequestMethod(HttpConnection.POST);
+                    OutputStream out = hc.openOutputStream();
+
+                    //Base64 b = new Base64();
+                    //     String imageString = b.encodeToString(imageByteOutdoor);
+                    //  String imageString = base64Encode(imageByteOutdoor);
+                    //  out.write(imageString.getBytes());
+
+                    is = hc.openInputStream();
+                    int ch = is.read();
+                    while (ch != -1) {
+                        serverMsg.append((char) ch);
+
+                        ch = is.read();
+                    }
+                    System.out.println("Submit Report Indoors THREAD: " + serverMsg.toString().trim());
+                    reportIndoorServerMsg = serverMsg.toString().trim();
+                    // out.flush();
+                    out.close();
+                    is.close();
+                    hc.close();
+
+                    if (reportIndoorServerMsg.equals("T")) {
+
+                        reportIndoorServerMsg = "";
+                        indoorPostalStringItem.setText("");
+                        alert = new Alert("Success", "Report has been successfully submitted.", null, AlertType.CONFIRMATION);
+                        alert.setTimeout(2000); //Timeout in 2 seconds
+                        switchDisplayable(alert, getMainMenuForm());
+                    }
+
+                    if (reportIndoorServerMsg.equals("F")) {
+                        reportIndoorServerMsg = "";
+
+                        alert = new Alert("Error", "Unable to post report.", null, AlertType.ERROR);
+                        alert.setTimeout(2000); //Timeout in 2 seconds
+                        switchDisplayable(alert, getIndoorReportForm());
+                    }
+                } catch (Exception e) {
+                    System.out.println("Error in Submitting Report for Indoors.");
+                    e.printStackTrace();
+                }
 
 
             }
@@ -1211,6 +1330,7 @@ public class AmityMIDlet extends MIDlet implements CommandListener, ItemCommandL
                     imageByteOutdoor = raw;
 
                     String fileName = "AmityImage_" + d.getTime() + ".jpg";
+                    imageName = fileName;
 
 //                    Image thumb = createThumbnail(image);
 //
@@ -1267,7 +1387,7 @@ public class AmityMIDlet extends MIDlet implements CommandListener, ItemCommandL
                     imageByteIndoor = raw;
 
                     String fileName = "AmityImage_" + d.getTime() + ".jpg";
-
+                    imageName = fileName;
 //                    Image thumb = createThumbnail(image);
 //
 //                    // Place it in the main form.
