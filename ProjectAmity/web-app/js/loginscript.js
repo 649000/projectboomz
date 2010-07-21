70a86a318073d802ec849bd3ae7308ac2c2d5ece
nazri610 <nazri610@3847c4ab-4fc7-fcde-3ce7-b0303817ffbf>
2010-07-21 15:05:10 +0000

    For logging in

---
+++ ProjectAmity/web-app/js/loginscript.js
@@ -0,0 +1,30 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function checkLogin(response)
+{
+    var nric = $F("nric");
+    var password = $F("password");
+    var temp = response.responseText
+
+    if(nric != '' || password != '')
+        {
+        if (temp =="Success")
+    {
+        alert("Success")
+        //redirect somewhere lah or use remote fuction and call other method lah
+        //${remoteFunction(controller:'report', action:'index')}
+    }
+    else if (temp == "Invalid Password")
+    {
+        alert("Invalid Password")
+    
+    } else if (temp == "Invalid Login ID")
+    {
+        alert("Invalid Login ID")
+    }
+        }
+        else
+            alert("Login ID or Password Cannot be blank")
+}
